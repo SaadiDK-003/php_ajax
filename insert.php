a1a07d87ca50ff06f50b6e4d0db20e07771a2f56
SaadiDK-003 <pcremote059@gmail.com>
2022-09-07 03:19:56 +0500

    learn and fun

--- insert.php
+++ insert.php
@@ -1,55 +1,63 @@
 <?php
 require_once 'db/database.php';
 
-if(isset($_POST['title'])){
+if (isset($_POST['title'])) {
     $user = $_POST['title'];
     $img = $_POST['img'];
     $color = $_POST['color'];
-    $file = $_FILES['file']['tmp_name'];
+    // $file = $_FILES['file']['tmp_name'];
 
     $insert = $db->query("INSERT INTO `content` (Title,image,color) VALUES('$user','$img','$color')");
 
-    if($insert){
+    if ($insert) {
         $msg = 'User has been added Successfully.';
         echo json_encode($msg);
     }
-}
-else{
+} else {
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Item</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <!-- Header -->
-    <?php include_once 'includes/header.php';?>
-    <!-- Content -->
-    <div class="main">
-        <section class="add-user">
-            <h3 class="msg"></h3>
-            <form id="myForm" enctype="multipart/form-data">
-                <label for="title">Title</label>
-                <input type="text" name="title" id="title" required>
-                <label for="img">Img Name with extension</label>
-                <input type="text" name="img" id="img">
-                <label for="color">Color Code</label>
-                <input type="text" name="color" id="color">
-                <label for="file">File</label>
-                <input type="file" name="file" id="file">
-                
-                 <input type="submit" value="submit">
-            </form>
-        </section>
-    </div>
+    <!DOCTYPE html>
+    <html lang="en">
 
-    <?php include_once 'includes/footer.php';?>
-</body>
-</html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Add Item</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <!-- Header -->
+        <?php include_once 'includes/header.php'; ?>
+        <!-- Content -->
+        <div class="main">
+            <section class="add-user">
+                <h3 class="msg"></h3>
+                <form id="myForm" enctype="multipart/form-data">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" id="title" required>
+                    <label for="img">Select Image</label>
+                    <!-- <input type="text" name="img" id="img"> -->
+                    <select name="img" id="img">
+                        <option value="335x300_1.jpg">Sea View</option>
+                        <option value="335x300_2.jpg">Store</option>
+                        <option value="fff.png">Black BG</option>
+                        <option value="fff2.png">SeaGreen BG</option>
+                    </select>
+                    <label for="color">Color Code</label>
+                    <input type="text" name="color" id="color">
+                    <!-- <label for="file">File</label>
+                    <input type="file" name="file" id="file"> -->
+
+                    <input type="submit" value="submit">
+                </form>
+            </section>
+        </div>
+
+        <?php include_once 'includes/footer.php'; ?>
+    </body>
+
+    </html>
 
 <?php } ?>
