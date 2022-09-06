50757be7ec09bd9588528250aee7bc68c8865bba
SaadiDK-003 <pcremote059@gmail.com>
2022-09-06 19:33:07 +0500

    php_work ~ images and color dynamically
    css update

--- delete.php
+++ delete.php
@@ -2,7 +2,7 @@
 require_once 'db/database.php';
 $id = $_POST['del'];
 
-$del = $db->query("DELETE FROM `user` WHERE `id`='$id'");
+$del = $db->query("DELETE FROM `content` WHERE `id`='$id'");
 
 if($del){
     echo 'item deleted.';
